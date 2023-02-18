2eaa524f19a17a8461c97f8e79d5219da3b04aff
fairous <fairous@gmail.com>
2023-02-18 13:36:46 +0530

    changes in csrf debug true

--- stockupdate/settings.py
+++ stockupdate/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x3$uir(fz#m-n+^lcwcvz0__qp=7cm^2pv4wrt4)b4n!#fij7m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
